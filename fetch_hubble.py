dab1b6940721b7fc5547466df99ef23fa30a6031
Nurbek Kuspanov <nurbek@inbox.ru>
2019-06-07 11:51:47 +0400

    deleted progressbar

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -2,15 +2,6 @@ import os
 import requests
 from dotenv import load_dotenv
 
-def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
-    iteration = min(total, iteration)
-    percent = "{0:.1f}"
-    percent = percent.format(100 * (iteration / float(total)))
-    filled_length = int(length * iteration // total)
-    pbar = fill * filled_length + zfill * (length - filled_length)
-    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
-        
-
 def get_file_extension(url):
     return url.split('.')[-1:][0]       
 
@@ -22,7 +13,6 @@ def fetch_hubble_images(url, collections, path):
         if response.ok:
             response = response.json()
             hubble_images_ids = [image_info['id'] for image_info in response]
-            iteration = len(hubble_images_ids)
             for image_id in hubble_images_ids:
                 image_url = 'http://hubblesite.org/api/v3/image/' 
                 response = requests.get(image_url + '{}'.format(image_id))
@@ -38,10 +28,7 @@ def fetch_hubble_images(url, collections, path):
                     else:
                         print('URL is not available')
                 else:
-                    print('URL is not available') 
-                print(render_progressbar(len(hubble_images_ids), iteration))
-                print('\u001b[2A')
-                iteration -= 1      
+                    print('URL is not available')       
         else:
             print('URL is not available')
     print('Done!' + ' ' * 40)
@@ -53,6 +40,6 @@ if __name__ == '__main__':
       os.makedirs(path)
     
     hubble_images_url = 'http://hubblesite.org/api/v3/images/'
-    collections = ['holiday_cards', 'wallpaper'] #['holiday_cards', 'wallpaper', 'spacecraft', 'news', 'printshop', 'stsci_gallery']
+    collections = ['holiday_cards', 'wallpaper']
     fetch_hubble_images(hubble_images_url, collections, path)
     
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -2,20 +2,11 @@ import os
 import requests
 from dotenv import load_dotenv
 
-def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
-    iteration = min(total, iteration)
-    percent = "{0:.1f}"
-    percent = percent.format(100 * (iteration / float(total)))
-    filled_length = int(length * iteration // total)
-    pbar = fill * filled_length + zfill * (length - filled_length)
-    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
-
 def fetch_spacex_last_launchage(url, path):
     print('Downloading from spacex site...')
     response = requests.get(url)
     if response.ok:
       images_urls = [url for url in response.json()['links']['flickr_images']]
-      iteration = len(images_urls)
       for index, url in enumerate(images_urls):
         filename = path + '/' + 'space_{}.jpg'.format(index+1)
         response = requests.get(url)
@@ -23,10 +14,7 @@ def fetch_spacex_last_launchage(url, path):
           with open(filename, 'wb') as file:
             file.write(response.content)
         else:
-          print('URL is not available')
-        print(render_progressbar(len(images_urls), iteration))
-        print('\u001b[2A')
-        iteration -= 1 
+          print('URL is not available') 
     else:
       print('URL is not available')
     print('Done!' + ' ' * 40)
